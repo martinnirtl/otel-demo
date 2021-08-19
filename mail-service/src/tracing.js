00624a11135e2a5e3cca50f312c55b3c43392c9d
Martin Nirtl <martin@nirtl.rocks>
2021-08-19 17:14:23 +0200

    fix tracing setup/configuration

--- mail-service/src/tracing.js
+++ mail-service/src/tracing.js
@@ -1,20 +1,24 @@
-const { BasicTracerProvider, SimpleSpanProcessor, ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
+const { SimpleSpanProcessor, ConsoleSpanExporter, BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base'); // exchange if possible
+const { NodeTracerProvider,  } = require('@opentelemetry/sdk-trace-node');
 const { IORedisInstrumentation } = require('@opentelemetry/instrumentation-ioredis');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 // const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector');
 const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector-proto');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const { SemanticResourceAttributes,  } = require('@opentelemetry/semantic-conventions');
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
+const { Resource } = require('@opentelemetry/resources');
+
+console.log('initializing tracing module...')
 
 const collectorOptions = {
   url: process.env.OTEL_BACKEND_URL, // url is optional and can be omitted - default is http://localhost:55681/v1/trace
   headers: {
-    Authorization: `Api-Token ${process.env.OTEL_AUTH}`,
+    Authorization: process.env.OTEL_AUTH_HEADER,
   },
 };
 
-const tracerProvider = new BasicTracerProvider({
+const tracerProvider = new NodeTracerProvider({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: 'mail-service',
   }),
