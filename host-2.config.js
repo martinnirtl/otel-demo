f34d6a08390941746e1372060752d73db4cf1ab8
Martin Nirtl <martin@nirtl.rocks>
2021-08-23 22:00:37 +0200

    update pm2 file

--- host-2.config.js
+++ host-2.config.js
@@ -12,6 +12,7 @@ module.exports = {
         "REDIS_PORT": 6379,
         "REDIS_DB": 1,
         "TEMPLATE_SERVICE_BASE_URL": "http://172.31.32.248:4200",
+        "OTEL_EXPORT_ENABLE": true,
         "OTEL_BACKEND_URL": "https://<tenant-url>/api/v2/otlp/v1/traces",
         "OTEL_AUTH_HEADER": "Api-Token <token>",
       }
