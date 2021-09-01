7de15f244658fe04cd841e87533cd4fc8581e685
Martin Nirtl <martin@nirtl.rocks>
2021-09-01 22:25:48 +0200

    add logs

--- loadgen/src/index.js
+++ loadgen/src/index.js
@@ -2,6 +2,8 @@ const faker = require('faker');
 const axios = require('axios');
 
 setInterval(async () => {
-  await axios.post(`${process.env.BACKEND_BASE_URL}/subscribe`, { email: faker.internet.email() });
+  const email = faker.internet.email();
 
+  console.log('subscribing with email: ' + email);
+  await axios.post(`${process.env.BACKEND_BASE_URL}/subscribe`, { email });
 }, 60000);
