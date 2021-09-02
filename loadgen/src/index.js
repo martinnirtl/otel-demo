b2d3e94f72504c0c7db685ad9e3c60f4847c6833
Martin Nirtl <martin@nirtl.rocks>
2021-09-02 20:22:54 +0200

    add logic for signup

--- loadgen/src/index.js
+++ loadgen/src/index.js
@@ -2,8 +2,13 @@ const faker = require('faker');
 const axios = require('axios');
 
 setInterval(async () => {
-  const email = faker.internet.email();
+  console.log('generating user...');
+  const user = {
+    email: faker.internet.email,
+    password: faker.internet.password(),
+    name: `${faker.name.firstName()} ${faker.name.lastName()}`
+  };
 
-  console.log('subscribing with email: ' + email);
-  await axios.post(`${process.env.BACKEND_BASE_URL}/subscribe`, { email });
+  console.log(`signing up ${user.email}...`);
+  await axios.post(`${process.env.BACKEND_BASE_URL}/signUp`, user);
 }, 60000);
