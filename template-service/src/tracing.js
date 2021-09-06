f7628cab1d413f142960cbb8df744b051fe7fb3e
Martin Nirtl <martin@nirtl.rocks>
2021-09-06 21:53:55 +0200

    add auto-instrumentation for redis

---
+++ template-service/src/tracing.js
@@ -0,0 +1,15 @@
+// const opentelemetry = require('@opentelemetry/api');
+const { BasicTracerProvider } = require('@opentelemetry/tracing');
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const { IORedisInstrumentation } = require('@opentelemetry/instrumentation-ioredis');
+const { logger } = require('./logging');
+
+logger.info('initializing the tracing module...');
+
+const tracerProvider = new BasicTracerProvider();
+tracerProvider.register();
+
+registerInstrumentations({
+  tracerProvider,
+  instrumentations: [new IORedisInstrumentation()],
+});
