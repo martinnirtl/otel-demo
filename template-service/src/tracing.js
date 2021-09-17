2319f8778e3e8b98a78912f65c14db699c2690d9
Martin Nirtl <martin@nirtl.rocks>
2021-09-17 10:07:46 +0200

    fix tracing setup

--- template-service/src/tracing.js
+++ template-service/src/tracing.js
@@ -1,14 +1,14 @@
-const { BasicTracerProvider } = require('@opentelemetry/tracing');
+// const { BasicTracerProvider } = require('@opentelemetry/tracing');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { IORedisInstrumentation } = require('@opentelemetry/instrumentation-ioredis');
 const { logger } = require('./logging');
 
 logger.info('initializing the tracing module...');
 
-const tracerProvider = new BasicTracerProvider();
-tracerProvider.register();
+// const tracerProvider = new BasicTracerProvider();
+// tracerProvider.register();
 
 registerInstrumentations({
-  tracerProvider,
+  // tracerProvider,
   instrumentations: [new IORedisInstrumentation()],
 });
