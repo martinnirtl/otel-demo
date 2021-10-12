697e1fe32c9a4483acf0a4465c1c959e4be58758
Martin Nirtl <martin@nirtl.rocks>
2021-10-12 21:17:32 +0200

    default port 4000

--- backend/src/index.js
+++ backend/src/index.js
@@ -117,7 +117,7 @@ app.post('/signup', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () => logging.logger.info(`listening on port ${port}`));
 
 exitHook(async () => {
