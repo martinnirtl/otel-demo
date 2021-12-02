ba1de1601799df1de31fa1930b7d41d0afb41e42
Martin Nirtl <martin@nirtl.rocks>
2021-12-02 22:28:39 +0100

    add diag logger

--- services/template-service/src/index.js
+++ services/template-service/src/index.js
@@ -1,9 +1,12 @@
 const PROTO_PATH = __dirname + '/../protos/template-service.proto';
 
+const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const { log } = require('./logging');
 const { render } = require('./renderer');
+
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
 
 log.info(`loading the proto file from: ${PROTO_PATH}`);
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
