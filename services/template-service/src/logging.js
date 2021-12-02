a3fcad412bf2c7688ff2d04666da96d7f3408b83
Martin Nirtl <martin@nirtl.rocks>
2021-12-02 22:40:02 +0100

    disable nestedKey

--- services/template-service/src/logging.js
+++ services/template-service/src/logging.js
@@ -2,7 +2,7 @@ const pino = require('pino')({
   name: process.env.SERVICE_NAME,
   base: undefined,
   messageKey: 'content',
-  nestedKey: 'meta',
+  // nestedKey: 'meta', // NOTE allow dt.trace_id and dt.span_id to be top level children
   formatters: {
     level(label) {
       return { level: label };
