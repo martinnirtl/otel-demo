9aa37609898b6e6b1773394881efa25859529931
Martin Nirtl <martin@nirtl.rocks>
2021-12-03 21:27:19 +0100

    clean up otel instrumentation

--- services/template-service/src/tracing.js
+++ services/template-service/src/tracing.js
@@ -1,15 +1,10 @@
-// const { BasicTracerProvider } = require('@opentelemetry/tracing');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { IORedisInstrumentation } = require('@opentelemetry/instrumentation-ioredis');
 const { GrpcInstrumentation } = require('@opentelemetry/instrumentation-grpc');
-const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 const { log } = require('./logging');
 
 log.info('initializing the tracing module...');
 
-// const tracerProvider = new BasicTracerProvider();
-// tracerProvider.register();
-
 registerInstrumentations({
-  instrumentations: [new IORedisInstrumentation(), new GrpcInstrumentation(), new HttpInstrumentation()],
+  instrumentations: [new IORedisInstrumentation(), new GrpcInstrumentation()],
 });
