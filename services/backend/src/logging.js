c2862ebed8aa0a76ce4f91ac0ac019c4cfd41a66
Martin Nirtl <martin@nirtl.rocks>
2021-12-08 21:44:44 +0100

    update logging configuration

--- services/backend/src/logging.js
+++ services/backend/src/logging.js
@@ -1,12 +1,18 @@
+const { v4: uuid } = require('uuid');
 const pino = require('pino-http')({
-  // name: process.env.SERVICE_NAME,
-  // base: undefined,
+  name: process.env.SERVICE_NAME,
+  base: undefined,
   messageKey: 'content',
   // nestedKey: 'meta', // NOTE allow dt.trace_id and dt.span_id to be top level children
   formatters: {
     level(label) {
       return { level: label };
     },
+  },
+  genReqId(req) {
+    const traceparent = req.headers['traceparent'];
+
+    return traceparent ? traceparent.split('-')[1] : uuid();
   },
   level: process.env.LOG_LEVEL || 'debug',
   quietReqLogger: true,
