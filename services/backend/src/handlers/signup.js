102fb3d8b9242c11c491a71f30cbc943c13d9eda
Martin Nirtl <martin@nirtl.rocks>
2021-12-10 23:37:46 +0100

    cleanup code and add otel instrumentation

--- services/backend/src/handlers/signup.js
+++ services/backend/src/handlers/signup.js
@@ -1,33 +1,40 @@
-const opentelemetry = require('@opentelemetry/api');
+const { context, trace, SpanStatusCode } = require('@opentelemetry/api');
 // const { CounterMetric } = require('@opentelemetry/metrics');
 
 const _ = require('lodash');
 const axios = require('axios').default;
 
-const tracer = opentelemetry.trace.getTracer('handlers/signup.js');
+const tracer = trace.getTracer('handlers/signup.js');
 
 module.exports = async (req, res) => {
-  const user = _.get(req, 'body');
-  req.log.debug({ user }, 'signing up new user');
+  req.log.debug({ headers: req.headers }, 'signing up new user');
 
+  const user = _.get(req, 'body'); // FYI usually we would need to validate the user object - we will only validate the email via a simple regex below
+
+  // INSTRUMENT email validation [simple]
+  let span = tracer.startSpan('validate email', { attributes: { 'app.user.email': user.email } });
   try {
-    // TODO verify email by regex
-    const valid = true;
+    const simpleMailRegex = /\S+@\S+\.\S+/;
+    const valid = simpleMailRegex.test(user.email);
     req.log.debug('isValidEmail: ' + valid);
 
     if (!valid) {
+      span.addEvent('validation failed');
+
       throw new Error('Invalid email detected');
     }
   } catch (error) {
+    span.setStatus(SpanStatusCode.ERROR);
     req.log.error(error);
 
     return res.status(400).send({ code: 'InvalidEmail' });
+  } finally {
+    span.end();
   }
 
   try {
     const users = req.db.collection('users');
-
-    const operationResult = await users.insertOne(user); // we store passwords in plain text (which is bad), but ok for demo purposes!
+    const operationResult = await users.insertOne(user); // FYI we store passwords in plain text (which is bad), but ok for demo purposes!
     req.log.debug({ operationResult }, 'created new user');
   } catch (error) {
     req.log.info(error);
@@ -35,16 +42,13 @@ module.exports = async (req, res) => {
     return res.status(400).send({ code: 'CreateUserError' });
   }
 
-  // const currentSpan = opentelemetry.trace.getSpan(opentelemetry.context.active());
-  // req.log.info(`traceid: ${currentSpan.spanContext().traceId}`);
-
-  // const span = tracer.startSpan('Send newsletter subscription mail', { attributes: req.body });
-  // const ctx = opentelemetry.trace.setSpan(opentelemetry.context.active(), sendConfirmationEmailSpan);
-
+  // INSTRUMENT sending email [advanced] - nested traces
+  span = tracer.startSpan('sending email', { attributes: { 'app.user.email': user.email } });
+  const ctx = trace.setSpan(context.active(), span);
   try {
     req.log.info('building the payload...');
-    const span = tracer.startSpan('Build payload', { attributes: { 'app.user.email': user.email } });
 
+    const subspan = tracer.startSpan('building the payload', null, ctx);
     const emailContent = {
       to: user.email,
       from: 'welcome@nptn.one',
@@ -56,28 +60,37 @@ module.exports = async (req, res) => {
         },
       },
     };
+    subspan.end();
 
     req.log.info('sending the email...');
-    const { data } = await axios.post(`${process.env.MAIL_SERVICE_BASE_URL}/send`, emailContent);
-    req.log.debug({ data }, 'done');
+    const { data } = await context.with(ctx, async () => {
+      const subspan = tracer.startSpan('calling mail-service', {
+        attributes: { 'app.mail-service': `${process.env.MAIL_SERVICE_BASE_URL}/send` },
+      });
+      try {
+        return axios.post(`${process.env.MAIL_SERVICE_BASE_URL}/send`, emailContent);
+      } catch (error) {
+        subspan.setStatus(SpanStatusCode.ERROR);
+        req.log.error(error);
 
-    // sendConfirmationEmailSpan.addEvent('send', emailContent);
-    // sendConfirmationEmailSpan.setStatus({
-    //   code: opentelemetry.SpanStatusCode.OK,
-    // });
-    // sendConfirmationEmailSpan.end();
-    span.end();
+        throw new Error('sending the email failed');
+      } finally {
+        subspan.end();
+      }
+    });
+    // const { data } = await axios.post(`${process.env.MAIL_SERVICE_BASE_URL}/send`, emailContent);
+    req.log.debug({ res: data }, 'done');
 
     return res.end();
   } catch (error) {
+    span.setStatus({
+      code: SpanStatusCode.ERROR,
+      message: error.message,
+    });
     req.log.error(error);
 
-    // sendConfirmationEmailSpan.setStatus({
-    //   code: opentelemetry.SpanStatusCode.ERROR,
-    //   message: error.message
-    // });
-    // sendConfirmationEmailSpan.end();
-
     return res.status(500).send();
+  } finally {
+    span.end();
   }
 };
--- services/mail-service/src/handlers/send.js
+++ services/mail-service/src/handlers/send.js
@@ -1,77 +1,122 @@
-const opentelemetry = require('@opentelemetry/api');
+const { context, trace, propagation, SpanStatusCode } = require('@opentelemetry/api');
 const _ = require('lodash');
 const axios = require('axios').default;
 const { nanoid } = require('nanoid');
 
 const { client: templateService } = require('../templateService');
 
-const tracer = opentelemetry.trace.getTracer('handers/send.js');
+const tracer = trace.getTracer('handers/send.js');
 
 module.exports = async (req, res) => {
-  req.log.debug(req.headers);
-  // req.log.info(api.context.active());
+  req.log.info({ headers: req.headers }, 'processing mail');
+  req.log.debug({ ctx: trace.getSpanContext(context.active()) }, 'active otel context');
+
   const sid = _.get(req, 'body.sid', nanoid(10));
 
+  // INSTRUMENT extracting variables [simple]
   const span = tracer.startSpan('Extracting variables', { attributes: { 'app.mail.sid': sid } });
-
   const template = _.get(req, 'body.template');
   let subject = _.get(req, 'body.subject');
   let text = _.get(req, 'body.text');
 
   span.end();
 
-  try {
-    const baggage = opentelemetry.propagation.createBaggage({ hello: { value: 'world', metadata: ['foo', 'bar'] } });
-    let context = opentelemetry.context.active();
-    context = opentelemetry.propagation.setBaggage(context, baggage);
-    const headers = {};
-    opentelemetry.propagation.inject(context, headers);
+  // INSTRUMENT render template [advanced] - add random baggage
+  const baggage = propagation.createBaggage({ hello: { value: 'world', metadata: ['foo', 'bar'] } });
+  const ctx = propagation.setBaggage(context.active(), baggage);
+  // const headers = {};
+  // propagation.inject(context, headers);
 
-    if (template) {
-      req.log.info(template, 'calling template-service to render text...');
-      // text = await axios.post(process.env.TEMPLATE_SERVICE_BASE_URL + '/render', { template }, { headers });
-      const renderedTemplate = await new Promise((resolve, reject) =>
-        templateService.render(template, (error, response) => {
-          req.log.info({ error, response }, 'received response');
+  let renderedTemplate;
+  if (template) {
+    renderedTemplate = tracer.startActiveSpan(
+      'render template',
+      { attributes: { 'app.template.name': template.name } },
+      ctx,
+      async span => {
+        req.log.info(template, 'calling template-service to render text...');
 
-          if (error || !response.success) {
-            return reject(error || new Error('Invokation of template-service failed!'));
-          }
+        try {
+          const { subject, body } = await new Promise((resolve, reject) =>
+            templateService.render(template, (error, response) => {
+              req.log.info({ error, response }, 'received response');
 
-          return resolve(response);
-        }),
-      );
+              if (error || !response.success) {
+                return reject(error || new Error('Invokation of template-service failed!'));
+              }
 
-      subject = renderedTemplate.subject;
-      text = renderedTemplate.body;
-    }
+              return resolve(response);
+            }),
+          );
 
-    if (!text) {
-      throw new Error('Empty text field is not allowed');
-    }
+          return { subject, body };
+        } catch (error) {
+          span.setStatus({
+            code: SpanStatusCode.ERROR,
+            message: error.message,
+          });
+          req.log.error(error);
 
-    req.log.info('sending mail payload to mail-provider...');
-    const { data } = await axios.post(`https://httpbin.org/anything`, {
-      data: {
-        subject,
-        text,
+          return null;
+        } finally {
+          span.end();
+        }
       },
-      headers,
-    });
-    req.log.info(data, 'mail sent');
-    // req.log.info(data);
+    );
+  }
 
-    if (sid) {
-      req.log.info('persisting status in db...');
-
-      req.cache.setex(sid, 86400, 'accepted').catch(error => req.log.error(error));
-    }
-
-    req.log.info('sending response...');
-    return res.status(200).send({ status: 'accepted', sid });
-  } catch (error) {
-    req.log.error(error);
+  if (!renderedTemplate) {
+    req.log.error('rendering failed');
 
     return res.status(500).send({ status: 'failed', sid, code: 'SendingFailed', message: 'Failed to send email' });
   }
+
+  const data = await tracer.startActiveSpan(
+    'deliver mail',
+    {
+      attributes: { 'app.mail.external': 'https://httpbin.org/anything' },
+    },
+    ctx,
+    async span => {
+      try {
+        req.log.info('sending mail payload to mail-provider...');
+
+        // FYI baggage should be propagated automatically as it's set on the active context
+        const { data } = await axios.post('https://httpbin.org/anything', {
+          data: {
+            subject,
+            text,
+          },
+          // headers,
+        });
+        req.log.info({ res: data }, 'mail sent');
+
+        if (sid) {
+          req.log.info('persisting status in db...');
+
+          req.cache.setex(sid, 86400, 'accepted').catch(error => req.log.error(error));
+        }
+
+        return data;
+      } catch (error) {
+        span.setStatus({
+          code: SpanStatusCode.ERROR,
+          message: error.message,
+        });
+        req.log.error(error);
+
+        return null;
+      } finally {
+        span.end();
+      }
+    },
+  );
+
+  if (!data) {
+    req.log.error('sending failed');
+    return res.status(500).send({ status: 'failed', sid, code: 'SendingFailed', message: 'Failed to send email' });
+  }
+
+  req.log.info('sending response...');
+  return res.status(200).send({ status: 'accepted', sid });
 };
